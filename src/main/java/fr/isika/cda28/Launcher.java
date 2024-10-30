2a297d85f20f77ecc8df0b7400562b7ca458a977
AnthonySk <anthony.statek@gmail.com>
2024-10-30 16:31:31 +0100

    bonjour depuis john doe

--- src/main/java/fr/isika/cda28/Launcher.java
+++ src/main/java/fr/isika/cda28/Launcher.java
@@ -3,6 +3,6 @@ package fr.isika.cda28;
 public class Launcher {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World John Doe");
 	}
 }
